d9389bd49a58eab4f93d9d03ae37d075c464f6c3
Lucas Fernandes <Lukasfcoelho@outlook.com>
2024-10-17 19:44:44 -0300

    adicionado classe user

---
+++ LibraryTJRJ.Domain/Users/User.cs
@@ -0,0 +1,25 @@
+﻿using LibraryTJRJ.Domain.Abstractions;
+
+namespace LibraryTJRJ.Domain.Users;
+
+public class User : Entity
+{
+    public string FirstName { get; } = null!;
+    public string LastName { get; } = null!;
+    public string Email { get; } = null!;
+    public string Password { get; } = null!;
+    public DateTime CreatedDateTime { get; }
+    public DateTime UpdatedDateTime { get; }
+
+    private User(Guid id, string firstName, string lastName, string email)
+    : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    private User()
+    {
+    }
+}
